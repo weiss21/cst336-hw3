609e949dfaa956fcddd181e1d9dbd37753b64249
waisalirobleh <waisalirobleh@gmail.com>
2019-07-15 01:37:41 -0400

    changed focus

--- js/nba.js
+++ js/nba.js
@@ -1,48 +1,33 @@
-var url = "https://stats.nba.com/stats/playergamelog?DateFrom=&DateTo=&LeagueID=00&PlayerID=201942&Season=2015-16&SeasonType=Regular+Season&format=jsonp";
-var token;
-var expiresAt = 0;
+$("#zip").on("change", function() {
+  $('#zipError').html("");
 
-//Listener
-window.onload = getPlayerGameLogs();
-
-
-
-$(document).ready(function() {
-  
-function getPlayerGameLogs() {
-  
-  $.ajax ({
+  $.ajax({
     method: "GET",
-    url: url,
-    dataType:"jsonp",
-    success: function(response, status) {
-      token = response.access_token;
-      expiresAt = Date.now() + response.expires_in - 60;
-      populateTable(response.resultSets, 'CommonPlayerInfo');
-      populateTable(response.resultSets, 'PlayerHeadlineStats');
-    }
-  })
-  
-}
-  getPlayerGameLogs();
-});
+    url: "https://cst336.herokuapp.com/projects/api/cityInfoAPI.php",
+    dataType: "json",
+    data: {
+      "zip": $("#zip").val()
+    },
+    success: function(result, status) {
+      if (result) {
+        $('#city').html(result.city);
+        $('#latitude').html(result.latitude);
+        $('#longitude').html(result.longitude);
+      } else {
+        $('#zipError').html("Zip code not found.");
+        $('#zipError').css("color", "red")
+        $('#city').html('');
+        $('#latitude').html('');
+        $('#longitude').html('');
+      }
+    },
+    error: function() {
+      $('#zipError').html("Zip code not found.");
+      $('#zipError').css("color", "red")
 
-function populateTable(resultSets, setName) {
-  var data = resultSets.filter(function(set){
-    return set.name === setName;
-  })[0];
-  var headers = data.headers;
-  var rowSet = data.rowSet;
-  var table = $('#' + setName);
-  var tr = table.append($('<tr>'));
-  $(headers).each(function(){
-    tr.append(tr.append($('<th>').text(this.toString())));
-  });
-  $(rowSet).each(function(){
-    var tr = $('<tr>');
-    this.forEach(function(item){
-      tr.append($('<td>').text(item.toString()));
-    });
-    table.append(tr);
-  });
-}
+      $('#city').html('');
+      $('#latitude').html('');
+      $('#longitude').html('');
+    }
+  }); // ajax
+});
